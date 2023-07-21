b419a33f6894fd30cb208aeb3077c7e1c9bb1f60
Allison01~ <farrella95@gmail.com>
2023-07-21 10:56:58 -0400

    yay it works!

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 require("dotenv").config()
 const express = require('express');
 const app = express();
-const port = 3000; // You can use any available port you prefer
+const port = 3000;
 const key = process.env.API_KEY;
 
-// Define a simple route
+
 app.use(express.static('public'));
 
-// Start the server
+app.get('/api/key', (req, res) => {
+    res.send(key);
+  });
+  
+  app.get('/app.js', (req, res) => {
+    res.sendFile(__dirname + '/public/app.js');
+  });
+
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
--- public/app.js
+++ public/app.js
@@ -1,35 +1,35 @@
-
 let mainImg = document.querySelector("img");
 let mainVid = document.querySelector("iframe");
 let btn = document.querySelector("button");
 
+fetch('/api/key')
+  .then(response => response.text())
+  .then(key => {
+    // apiKey = apiKey.replace(/(\r\n|\n|\r)/gm, '');
+    console.log(key);
 
-btn.addEventListener("click", () => {
+    btn.addEventListener("click", () => {
+      const choice = document.querySelector("input").value.toLowerCase();
+      const url = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${choice}`;
+      fetch(url)
+        .then((res) => res.json()) 
+        .then((data) => {
+          console.log(data);
+          if (data.media_type === "image") {
+            mainImg.src = data.hdurl;
+          } else if (data.media_type === "video") {
+            mainVid.src = data.url;
+            mainImg.classList.add("active");
+            mainVid.classList.add("active");
+          }
 
-  const choice = document.querySelector("input").value.toLowerCase();
-  const url = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${choice}`;
-  fetch(url)
-    .then((res) => res.json()) 
-    .then((data) => {
-      console.log(data);
-      if (data.media_type === "image") {
-        mainImg.src = data.hdurl;
-      } else if (data.media_type === "video") {
-        mainVid.src = data.url;
-        mainImg.classList.add("active");
-        mainVid.classList.add("active");
-      }
-
-      document.querySelector(
-        "h3"
-      ).innerText = `About this: ${data.explanation}`;
-    })
-
-    .catch((err) => {
-      console.log(`error ${err}`);
+          document.querySelector(
+            "h3"
+          ).innerText = `About this: ${data.explanation}`;
+        })
+        .catch((err) => {
+          console.log(`error ${err}`);
+        });
     });
-});
-
-
-
-
+  })
+  .catch(error => console.error('Error fetching API key:', error));
